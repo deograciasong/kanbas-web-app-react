459f90152bf9b55d1c08e97019a8acd8865f8fe4
deograciasong <77689676+deograciasong@users.noreply.github.com>
2024-10-08 13:27:44 -0700

    create branch

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -1,5 +1,9 @@
+import VariablesAndConstants from "./VariablesAndConstants";
 export default function Lab3() {
     return (
-        <h2>Lab3</h2>
-    )
+        <div id="wd-lab3">Í
+            <h3>Lab 3</h3>
+            <VariablesAndConstants />
+        </div>
+    );
 }

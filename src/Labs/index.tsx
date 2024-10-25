00552264806b9b6c17e5f2576abbd4bc2de6feb0
deograciasong <77689676+deograciasong@users.noreply.github.com>
2024-10-25 15:52:26 -0700

    labs complete

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -1,21 +1,27 @@
 import Lab1 from "./Lab1";
 import Lab2 from "./Lab2";
 import Lab3 from "./Lab3";
-import {Navigate, Routes, Route } from 'react-router-dom';
+import Lab4 from "./Lab4";
+import store from "./store";
+import { Provider } from "react-redux";
+import { Navigate, Routes, Route } from 'react-router-dom';
 import TOC from "./TOC";
 export default function Labs() {
     return (
-        <div id="wd-labs">
-            <h1>Deo Gracias Ong</h1>
-            <h3>Online - Thursday 7pm-10pm EST</h3>
-            <h1>Labs</h1>
-            <TOC />
-            <Routes>
-                <Route path="/" element={<Navigate to="Lab1" />} />
-                <Route path="Lab1" element={<Lab1/>} />
-                <Route path="Lab2" element={<Lab2/>} />
-                <Route path="Lab3/*" element={<Lab3/>} />
-            </Routes>
-        </div>
+        <Provider store={store}>
+            <div id="wd-labs">
+                <h1>Deo Gracias Ong</h1>
+                <h3>Online - Thursday 7pm-10pm EST</h3>
+                <h1>Labs</h1>
+                <TOC />
+                <Routes>
+                    <Route path="/" element={<Navigate to="Lab1" />} />
+                    <Route path="Lab1" element={<Lab1 />} />
+                    <Route path="Lab2" element={<Lab2 />} />
+                    <Route path="Lab3/*" element={<Lab3 />} />
+                    <Route path="Lab4/*" element={<Lab4 />} />
+                </Routes>
+            </div>
+        </Provider>
     );
 }

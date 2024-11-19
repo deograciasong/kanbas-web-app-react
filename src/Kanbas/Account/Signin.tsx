58897ca098fdfcba3846f79c0e7d2afd8b1aff2c
deograciasong <77689676+deograciasong@users.noreply.github.com>
2024-11-19 13:56:13 -0800

    lab completed

--- src/Kanbas/Account/Signin.tsx
+++ src/Kanbas/Account/Signin.tsx
@@ -2,20 +2,17 @@ import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { setCurrentUser } from "./reducer";
 import { useDispatch } from "react-redux";
-import * as db from "../Database";
-
+import * as client from "./client";
 export default function Signin() {
-    const [credentials, setCredentials] = useState<any>({});
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
-    const signin = () => {
-        const user = db.users.find(
-            (u: any) => u.username === credentials.username && u.password === credentials.password);
-        if (!user) return;
-        dispatch(setCurrentUser(user));
-        navigate("/Kanbas/Dashboard");
-    };
-
+  const [credentials, setCredentials] = useState<any>({});
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const signin = async () => {
+    const user =  await client.signin(credentials);
+    if (!user) return;
+    dispatch(setCurrentUser(user));
+    navigate("/Kanbas/Dashboard");
+  };
     return (
         <div id="wd-signin-screen">
             <h3>Sign in</h3>
@@ -29,4 +26,4 @@ export default function Signin() {
             <Link id="wd-signup-link" to="/Kanbas/Account/Signup"> Sign up</Link>
         </div>
     )
-}
+};

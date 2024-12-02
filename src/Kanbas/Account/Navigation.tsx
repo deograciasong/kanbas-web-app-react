f18b623d7bfdc2d4600cbc08f2e04b489b966ce4
deograciasong <77689676+deograciasong@users.noreply.github.com>
2024-12-01 18:57:00 -0800

    users and installing mongo

--- src/Kanbas/Account/Navigation.tsx
+++ src/Kanbas/Account/Navigation.tsx
@@ -3,15 +3,15 @@ import { useSelector } from "react-redux";
 export default function AccountNavigation() {
   const { currentUser } = useSelector((state: any) => state.accountReducer);
   const links = currentUser ? ["Profile"] : ["Signin", "Signup"];
+  const active = (path: string) => (pathname.includes(path) ? "active" : "");
+  const { pathname } = useLocation();
   return (
-    <div id="wd-account-navigation">
+    <div id="wd-account-navigation" className="list-group">
       {links.map((link) => (
-        <div>
-          <Link to={`/Kanbas/Account/${link}`} >
-            {link}
-          </Link>
-        </div>
+        <Link key={link} to={`/Kanbas/Account/${link}`} className={`list-group-item border-0 ${active(link)}`}> {link} </Link>
       ))}
+      {currentUser && currentUser.role === "ADMIN" && (
+        <Link to={`/Kanbas/Account/Users`} className={`list-group-item border-0 ${active("Users")}`}> Users </Link> )}
     </div>
   );
 }

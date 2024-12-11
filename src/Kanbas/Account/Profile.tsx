0abb35361fe3e0552f0047a75ba5de7082ac3f45
deograciasong <77689676+deograciasong@users.noreply.github.com>
2024-12-10 23:08:04 -0800

    safety net

--- src/Kanbas/Account/Profile.tsx
+++ src/Kanbas/Account/Profile.tsx
@@ -40,6 +40,7 @@ export default function Profile() {
                     <input defaultValue={profile.email} id="wd-email" className="form-control mb-2"
                         onChange={(e) => setProfile({ ...profile, email: e.target.value })} />
                     <select onChange={(e) => setProfile({ ...profile, role: e.target.value })}
+                        value={profile.role}
                         className="form-control mb-2" id="wd-role">
 
                         <option value="USER">User</option>
